b8a0aec3e4c6bf39adbd04d82c0c8f468e79684c
Henry Nguyen <nguyenhai1591@gmail.com>
2021-03-20 09:40:57 +1000

    Add db connection:

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(
+			`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-tutorial.r8t4y.mongodb.net/mern-tutorial?retryWrites=true&w=majority`,
+			{
+				useCreateIndex: true,
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false
+			}
+		)
+		console.log('MongoDB connected yay')
+	} catch (error) {
+		console.log(error.message)
+		process.exit(1)
+	}
+}
+
+connectDB()
 
 const app = express()
 
