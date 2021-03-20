c1c0d379640928a98a8fb79016d925e81173bdd4
Henry Nguyen <nguyenhai1591@gmail.com>
2021-03-20 11:14:40 +1000

    Get all posts for each user

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,6 +4,19 @@ const verifyToken = require('../middleware/auth')
 
 const Post = require('../models/Post')
 
+// @route GET api/posts
+// @desc Get posts
+// @access Private
+router.get('/', verifyToken, async (req, res) => {
+	try {
+		const posts = await Post.find({ user: req.userId })
+		res.json({ success: true, posts })
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+	}
+})
+
 // @route POST api/posts
 // @desc Create post
 // @access Private
