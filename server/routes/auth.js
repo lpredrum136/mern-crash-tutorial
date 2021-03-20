8ef632b8d7984cb5520f9cb22e4ad34b62e55660
Henry Nguyen <nguyenhai1591@gmail.com>
2021-03-20 10:36:09 +1000

    Done login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
 
-// @route POST api/users
+// @route POST api/auth/register
 // @desc Register user
 // @access Public
 router.post('/register', async (req, res) => {
@@ -33,6 +33,47 @@ router.post('/register', async (req, res) => {
 		// Return token
 		const accessToken = jwt.sign(
 			{ userId: newUser._id },
+			process.env.ACCESS_TOKEN_SECRET
+		)
+
+		// return
+		res.json({ accessToken })
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+	}
+})
+
+// @route POST api/auth/login
+// @desc Login user
+// @access Public
+router.post('/login', async (req, res) => {
+	const { username, password } = req.body
+
+	// Simple validation
+	if (!username || !password)
+		return res
+			.status(400)
+			.json({ success: false, message: 'Missing username and/or password' })
+
+	try {
+		// Check for existing user
+		const user = await User.findOne({ username })
+		if (!user)
+			return res
+				.status(400)
+				.json({ success: false, message: 'Incorrect username or password' })
+
+		// Username found
+		const passwordValid = await argon2.verify(user.password, password)
+		if (!passwordValid)
+			return res
+				.status(400)
+				.json({ success: false, message: 'Incorrect username or password' })
+
+		// All good, return token
+		const accessToken = jwt.sign(
+			{ userId: user._id },
 			process.env.ACCESS_TOKEN_SECRET
 		)
 
