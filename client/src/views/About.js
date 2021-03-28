930c95e00e887d8525903f1db25410f12c49fa6d
Henry Nguyen <nguyenhai1591@gmail.com>
2021-03-28 23:06:36 +1000

    About page

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,8 +1,20 @@
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 const About = () => {
 	return (
-		<>
-			<p>About</p>
-		</>
+		<Row className='mt-5'>
+			<Col className='text-center'>
+				<Button
+					variant='primary'
+					href='https://www.youtube.com/c/HenryWebDev'
+					size='lg'
+				>
+					Visit my channel for more tutorials
+				</Button>
+			</Col>
+		</Row>
 	)
 }
 
